3660da0b41eac3c3fd0ed963d891208ebabe69b9
DB_win7 <DB_win7@119.192.133.107>
2017-03-24 00:40:57 +0900

    email 인증 테스트

---
+++ test/src/main/java/test/controller/IndexController.java
@@ -0,0 +1,14 @@
+package test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index.do")
+	public String goIndex(){
+		return "index";		
+	}
+
+}
---
+++ test/src/main/java/userservice/UserService.java
@@ -0,0 +1,5 @@
+package userservice;
+
+public interface UserService {
+
+}
