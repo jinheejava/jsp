34c96f41cbe627e75c4fe6ab05b45b772680ad2d
user1 <user1@KH_H>
2017-03-30 09:51:45 +0900

    이메일 인증 성공

---
+++ test2/src/main/java/mail/SMTPAuthenticatior.java
@@ -0,0 +1,15 @@
+package mail;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class SMTPAuthenticatior extends Authenticator{
+	
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication() {
+		
+		return new PasswordAuthentication("jinhue1120","wlsgml1523");
+	}
+	
+
+}
